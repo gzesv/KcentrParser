b0839e31bf1cbecd4a779061b75e5043c5ed0823
gzevs <grom_zetom@mail.ru>
2024-02-11 22:04:20 +0400

    init commit

---
+++ KcentrParser/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KcentrParser.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
